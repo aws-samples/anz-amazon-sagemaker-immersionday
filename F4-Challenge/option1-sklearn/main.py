0a77925ea2c809603930ceed28d76c5acd755456
Romina Sharifpour <9032900+RominaUQ@users.noreply.github.com>
2022-06-22 07:29:57 +0000

    changed the model

--- F4-Challenge/option1-sklearn/main.py
+++ F4-Challenge/option1-sklearn/main.py
@@ -21,31 +21,35 @@ from sklearn.externals import joblib
 
 #training script
 if __name__ =='__main__':
+     #------------------------------- parsing input parameters (from command line)
+    print('extracting arguments')
     parser = argparse.ArgumentParser()
-    # Load arguments from CLI / environment variables:
+       
+   #TODO: RandomForest hyperparameters
+
+
+
+   # TODO: Data, model, and output directories
+
     
     
-   
-
-
-
+    
+    
     args = parser.parse_args()
     
 
     #TODO-Load your data (both training and test) from container filesystem into training / test data sets and split them to train/test features and lables
     
     
-    
-    #TODO- standardise the features before fitting them into our model( both for test and train features)  
-    
-    
-    #TODO- train the logistic regression model
+     
+    #TODO- fit the randomforest model
    
 
     #Save the model to the location specified by args.model_dir, using the joblib
     
 
 
+    
     
     
 #define a function that loads the model    
